10e4f1348fc391e3ec6722302d8740197c807630
jiteshbilunia121 <jiteshbilunia1234@gmail.com>
2025-05-28 16:08:43 +0530

    Updated cors config

--- src/main/java/org/example/notifcation_service/Config/WebConfig.java
+++ src/main/java/org/example/notifcation_service/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow requests from your frontend origin
+                .allowedOrigins("http://localhost:3000", "https://parkingfrontenddeployed.vercel.app") // Allow requests from your frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .exposedHeaders("Authorization") // Expose specific headers if needed
